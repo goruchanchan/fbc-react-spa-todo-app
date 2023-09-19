98237a5631609cba58b5002a0bb85dd8e68429f9
goruchan <ppcfr481@yahoo.co.jp>
2023-09-19 22:29:17 +0900

    add 大枠とfavicon追加

---
+++ todo-app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <h2>一覧</h2>
+    <div className="Board">
+      a
+    </div>
+    <h2>編集</h2>
+    <div className="Board">
+      b
+    </div>
+  </React.StrictMode>
+);
